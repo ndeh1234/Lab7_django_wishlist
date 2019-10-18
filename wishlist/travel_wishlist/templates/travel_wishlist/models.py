4562900cf8f580cc1ee8b03031691cce7ed6be5b
nhann <nhanndeh@yahoo.com>
2019-10-17 21:28:05 -0500

    Completed up to slide 80

---
+++ wishlist/travel_wishlist/templates/travel_wishlist/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Place(models.Model):
+    """
+    This model is created to store a location and whether or not it has been visited.
+    """
+    user = models.ForeignKey('auth.User', null=False, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    visited = models.BooleanField(default=False)
+    notes = models.TextField(blank=True, null=True)
+    date_visited = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='user_images/', blank=True, null=True)
+
+
+
+    def __str__(self):
+        photo_str = self.photo.url if self.photo else 'no photo'
+        return f'{self.pk}: {self.name} visited? {self.visited} on {self.date_visited}\nPhoto {photo_str}'
